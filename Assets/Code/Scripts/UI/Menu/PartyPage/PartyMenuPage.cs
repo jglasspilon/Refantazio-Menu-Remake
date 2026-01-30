422cd381cf0dc1c034eb29b5f80ae664dafe696c
Julian_Glass-Pilon <julian@astucemedia.com>
2026-01-30 15:36:01 -0500

    Created party menu page and all of its animations

---
+++ Assets/Code/Scripts/UI/Menu/PartyPage/PartyMenuPage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PartyMenuPage : MenuPage
+{
+    public override void Confirm()
+    {
+        
+    }
+
+    public override void CycleDown()
+    {
+        
+    }
+
+    public override void CycleUp()
+    {
+        
+    }
+
+    public override void ResetPage()
+    {
+        
+    }
+}
