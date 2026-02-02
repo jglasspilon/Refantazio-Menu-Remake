5a79c6cdc48438d0c84c7244d3055a09afccb466
Julian_Glass-Pilon <julian@astucemedia.com>
2026-02-02 15:37:50 -0500

    Created quest menu page and all of its animations

---
+++ Assets/Code/Scripts/UI/Menu/QuestPage/QuestMenuPage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class QuestMenuPage : MenuPage
+{
+    public override void Confirm()
+    {
+        
+    }
+
+    public override void CycleDown()
+    {
+        
+    }
+
+    public override void CycleUp()
+    {
+        
+    }
+
+    public override void ResetPage()
+    {
+        
+    }
+}
