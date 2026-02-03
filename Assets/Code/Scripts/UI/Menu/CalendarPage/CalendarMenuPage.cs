24c636a4755614a37a692a3c5c7ccf6228e3f983
Julian_Glass-Pilon <julian@astucemedia.com>
2026-02-03 15:49:03 -0500

    Created calendar page and all of its animations

---
+++ Assets/Code/Scripts/UI/Menu/CalendarPage/CalendarMenuPage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CalendarMenuPage : MenuPage
+{
+    public override void Confirm()
+    {
+        
+    }
+
+    public override void CycleDown()
+    {
+        
+    }
+
+    public override void CycleUp()
+    {
+        
+    }
+
+    public override void ResetPage()
+    {
+        
+    }
+}
