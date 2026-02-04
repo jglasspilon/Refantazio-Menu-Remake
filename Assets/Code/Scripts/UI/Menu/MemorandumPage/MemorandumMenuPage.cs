ffbebe576d1cbe30870ecfa0a8bcaedcfc4f2c97
Julian_Glass-Pilon <julian@astucemedia.com>
2026-02-04 13:09:43 -0500

    Created memorandum page and all of its animations

---
+++ Assets/Code/Scripts/UI/Menu/MemorandumPage/MemorandumMenuPage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MemorandumMenuPage : MenuPage
+{
+    public override void Confirm()
+    {
+        
+    }
+
+    public override void CycleDown()
+    {
+        
+    }
+
+    public override void CycleUp()
+    {
+        
+    }
+
+    public override void ResetPage()
+    {
+        
+    }
+}
