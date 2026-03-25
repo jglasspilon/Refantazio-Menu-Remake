e989f01418816f10981f8fbf0fc1fb2bb73d2b02
Julian_Glass-Pilon <julian@astucemedia.com>
2026-03-25 15:30:24 -0400

    Sped up main menu page animations. Disabled mouse selection in main menu. Added starting money and magla

--- Assets/Code/Scripts/Core/InventoryManagement/InventoryManager.cs
+++ Assets/Code/Scripts/Core/InventoryManagement/InventoryManager.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    [SerializeField]
-    private InventoryData m_inventoryData;
-
-    [SerializeField]
-    private InventoryEntryData[] m_startingInventory;
+    [SerializeField] private InventoryData m_inventoryData;
+    [SerializeField] private InventoryEntryData[] m_startingInventory;
+    [SerializeField] private int m_startingMoney, m_startingMag;
 
     void Awake()
     {
@@ -20,5 +18,8 @@ public class InventoryManager : MonoBehaviour
         {
             m_inventoryData.AddOrRemoveItem(entry.Item, entry.Count);
         }
+
+        m_inventoryData.Money.Apply(m_startingMoney);
+        m_inventoryData.Magla.Apply(m_startingMag);
     }
 }
